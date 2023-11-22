ec94303a28c743326e505a548f868f82ec58a065
Tami <tamisimes2@gmail.com>
2023-11-22 13:02:26 -0300

    segunda entrega

---
+++ animaciones.js
@@ -0,0 +1,145 @@
+let nombre = prompt("¡Hola! Indicanos cuál es tu nombre")
+
+while (!nombre.trim()) {
+    nombre = prompt("¡Hola! Indicanos cuál es tu nombre") || ""
+    nombre = nombre.toLowerCase()
+}
+
+function saludar() {
+    alert("¡Hola " + nombre + "! ¡Bienvenido a esta nueva experiencia!")
+}
+
+saludar();
+
+let diaDeNacimientoDelUsuario = parseInt(prompt("¿Qué día naciste?"))
+let mesDeNacimientoDelUsuario = parseInt(prompt("¿En qué mes naciste? (Indicanos el número del 1 al 12"))
+
+const signosDelZodiaco = [
+    { Signos: ["Cáncer", "Escorpio", "Piscis"], Elemento: "agua" },
+    { Signos: ["Géminis", "Libra", "Acuario"], Elemento: "aire" },
+    { Signos: ["Aries", "Leo", "Sagitario"], Elemento: "fuego" },
+    { Signos: ["Tauro", "Virgo", "Capricornio"], Elemento: "tierra" },
+]
+if ((mesDeNacimientoDelUsuario === 3 && diaDeNacimientoDelUsuario >= 21) || (mesDeNacimientoDelUsuario === 4 && diaDeNacimientoDelUsuario <= 19)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Aries"));
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Aries y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de fuego: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+
+if ((mesDeNacimientoDelUsuario === 4 && diaDeNacimientoDelUsuario >= 20) || (mesDeNacimientoDelUsuario === 5 && diaDeNacimientoDelUsuario <= 20)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Tauro"))
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Tauro y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de tierra: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+
+if ((mesDeNacimientoDelUsuario === 5 && diaDeNacimientoDelUsuario >= 21) || (mesDeNacimientoDelUsuario === 6 && diaDeNacimientoDelUsuario <= 20)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Géminis"))
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Géminis y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de aire: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+
+if ((mesDeNacimientoDelUsuario === 6 && diaDeNacimientoDelUsuario >= 21) || (mesDeNacimientoDelUsuario === 7 && diaDeNacimientoDelUsuario <= 22)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Cáncer"))
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Cáncer y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de agua: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+if ((mesDeNacimientoDelUsuario === 7 && diaDeNacimientoDelUsuario >= 23) || (mesDeNacimientoDelUsuario === 8 && diaDeNacimientoDelUsuario <= 22)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Leo"))
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Leo y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de fuego: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+
+if ((mesDeNacimientoDelUsuario === 8 && diaDeNacimientoDelUsuario >= 23) || (mesDeNacimientoDelUsuario === 9 && diaDeNacimientoDelUsuario <= 22)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Virgo"))
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Virgo y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de tierra: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+
+if ((mesDeNacimientoDelUsuario === 9 && diaDeNacimientoDelUsuario >= 23) || (mesDeNacimientoDelUsuario === 10 && diaDeNacimientoDelUsuario <= 22)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Libra"))
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Libra y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de aire: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+
+if ((mesDeNacimientoDelUsuario === 10 && diaDeNacimientoDelUsuario >= 23) || (mesDeNacimientoDelUsuario === 11 && diaDeNacimientoDelUsuario <= 21)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Escorpio"))
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Escorpio y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de agua: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+
+if ((mesDeNacimientoDelUsuario === 11 && diaDeNacimientoDelUsuario >= 22) || (mesDeNacimientoDelUsuario === 12 && diaDeNacimientoDelUsuario <= 21)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Sagitario"))
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Sagitario y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de fuego: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+
+if ((mesDeNacimientoDelUsuario === 12 && diaDeNacimientoDelUsuario >= 22) || (mesDeNacimientoDelUsuario === 1 && diaDeNacimientoDelUsuario <= 19)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Capricornio"))
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Capricornio y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de tierra: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+
+if ((mesDeNacimientoDelUsuario === 1 && diaDeNacimientoDelUsuario >= 20) || (mesDeNacimientoDelUsuario === 2 && diaDeNacimientoDelUsuario <= 18)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Acuario"))
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Acuario y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de aire: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+
+if ((mesDeNacimientoDelUsuario === 2 && diaDeNacimientoDelUsuario >= 19) || (mesDeNacimientoDelUsuario === 3 && diaDeNacimientoDelUsuario <= 20)) {
+    const signoDelUsuarioEncontrado = signosDelZodiaco.find(signo => signo.Signos.includes("Piscis"))
+
+    if (signoDelUsuarioEncontrado) {
+        alert(`Tu signo es Piscis y es de elemento ${signoDelUsuarioEncontrado.Elemento}. Estos son todos los signos de agua: ${signoDelUsuarioEncontrado.Signos.join(", ")}`)
+    } else {
+        alert("No podemos encontrar tus características.")
+    }
+}
+
+
+const todosLosSignos = signosDelZodiaco.map(signo => signo.Signos).flat().join(", ")
+
+alert(`Por si no sabías, todos los signos del Zodíaco son: ${todosLosSignos}`)
